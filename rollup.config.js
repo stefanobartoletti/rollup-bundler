eccd807465bfcff4e04dfcad12640921effde2d4
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-07-11 14:19:40 +0200

    add js plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,16 @@
- export default {
+import nodeResolve from '@rollup/plugin-node-resolve'; 
+import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+ 
+ 
+export default {
   input: 'main.js',
   output: {
     file: 'dist/js/scripts.js',
   },
   plugins: [
+    nodeResolve(),
+    commonjs(),
+    babel({ babelHelpers: 'bundled' }),
   ]
 };
