73a7f4a22ddd15af298ced0f0c396c41d1ba4362
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-07-11 14:54:47 +0200

    add babel & terser config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,15 @@
+// --- Import plugins ---
+
 import nodeResolve from '@rollup/plugin-node-resolve'; 
 import commonjs from '@rollup/plugin-commonjs';
 import { babel } from '@rollup/plugin-babel';
- 
+import { terser } from 'rollup-plugin-terser';
+
+// --- Variables ---
+
+const production = (process.env.NODE_ENV === 'production');
+
+// --- Rollup configuration ---
  
 export default {
   input: 'main.js',
@@ -12,5 +20,6 @@ export default {
     nodeResolve(),
     commonjs(),
     babel({ babelHelpers: 'bundled' }),
+    production && terser(),
   ]
 };
