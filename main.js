2e5a4a8cf39569ad1933869323dc2df40259cbbe
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-07-11 13:57:23 +0200

    add basic javascript config

---
+++ main.js
@@ -0,0 +1,3 @@
+// --- Import Javascript ---
+
+import './src/js/scripts'
---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+ export default {
+  input: 'main.js',
+  output: {
+    file: 'dist/js/scripts.js',
+  },
+  plugins: [
+  ]
+};
