aa2ab75c13aa168a3b337c34faa6954dd5e7abc0
stefanobartoletti <36746429+stefanobartoletti@users.noreply.github.com>
2022-07-12 08:42:07 +0000

    Deploying to gh-pages from @ stefanobartoletti/rollup-bundler@024b4947581efcd0cd839d5f99fe8defde9aa9f8 🚀

---
+++ scripts.js
@@ -0,0 +1,1 @@
+!function(){"use strict";const e=document.querySelector("html"),r=document.querySelector("#header-wrapper");r&&document.addEventListener("scroll",(function(){e.scrollTop>50?r.classList.add("header-scrolled"):r.classList.remove("header-scrolled")}))}();
