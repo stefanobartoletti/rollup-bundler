3432859d66b886e8d78d737c78e990242284c0c8
stefanobartoletti <36746429+stefanobartoletti@users.noreply.github.com>
2022-07-12 15:43:48 +0000

    Deploying to gh-pages from @ stefanobartoletti/rollup-bundler@88ba48a5ed833a9eb5995a63900046c6292d2675 🚀

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+// --- Import plugins ---
+
+import del from 'rollup-plugin-delete';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import styles from 'rollup-plugin-styles';
+import html from '@rollup/plugin-html';
+
+// --- Variables ---
+
+const production = (process.env.NODE_ENV === 'production');
+
+// --- Rollup configuration ---
+
+export default {
+  input: 'main.js',
+  output: {
+    file: 'dist/scripts.js',
+    format: 'iife',
+    assetFileNames: '[name][extname]'
+  },
+  plugins: [
+    del({ targets: './dist' }),
+    nodeResolve(),
+    commonjs(),
+    babel({ babelHelpers: 'bundled' }),
+    production && terser(),
+    styles({
+      mode: ['extract', 'style.css'],
+      minimize: !!production
+    }),
+    html({
+      publicPath: ''
+    })
+  ]
+};
