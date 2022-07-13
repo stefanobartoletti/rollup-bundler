d47202739c0da09386aa442929cfe2712e41f52d
Stefano Bartoletti <bartoletti.stefano@gmail.com>
2022-07-13 13:14:13 +0200

    add multiple rollup configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'standard'
   ],
   rules: {
+    'no-undef': ['warn'],
     semi: ['error', 'always']
   }
 };
---
+++ bundle.js
@@ -0,0 +1,6 @@
+// --- GSAP import & init ---
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
